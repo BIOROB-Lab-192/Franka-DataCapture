67d7d2e6eb61c93f8042efe745673852bbaa63f2
haider <haider_al@me.com>
2025-10-28 21:35:50 -0700

    Added 2 dummy functions

--- source/expression.py
+++ source/expression.py
@@ -1,1 +1,16 @@
 """Facial expression recognition functions. This will be using a trained pytorch model"""
+
+import random
+import time
+
+
+class Expression:
+    def __init__(self):
+        self.name = "expression"
+
+    def read(self):
+        return {
+            "timestampe": time.time(),
+            "data": {"happy/not": random.randint(0, 1), "valence/arousal": random.random()},
+            "source": self.name,
+        }
