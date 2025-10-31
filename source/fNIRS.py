f90ec85a746f276990c72fa3a022a6c9f8b27456
haider <haider_al@me.com>
2025-10-31 10:49:40 -0700

    empty template

--- source/fNIRS.py
+++ source/fNIRS.py
@@ -1,1 +1,14 @@
 """Functionlaity for capturing data using MNE python for NIRx devices"""
+
+import time
+
+class fNIRS:
+    def __init__(self):
+        self.name = "fnirs"
+
+    def read(self):
+        return {
+            "timestamp": time.time(),
+            "data": "stuff",
+            "source": self.name,
+        }
