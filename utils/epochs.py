43b551d07dc72bc81213fd5113678cf5d117d5f3
haider <haider_al@me.com>
2025-10-31 11:25:35 -0700

    Added default fieldnames

--- utils/epochs.py
+++ utils/epochs.py
@@ -1,14 +1,14 @@
 import CSV_writer
 
 class EpochWriter(CSV_writer.CSVWRiter):
-    def __init__(self, filepath, fields):
-        super().__init__(filepath, fields)
+    def __init__(self, filepath):
+        super().__init__(filepath, fields=["epoch_id", "start_time", "end_time", "label"])
 
     def mark_epoch(self, epoch_id, start_time, end_time, label):
         self.write_row({
             "epoch_id": epoch_id,
             "start_time": start_time,
             "end_time": end_time,
-            "label": label or ""
+            "label": label
         })
         #  send epoch signal to fNIRS
